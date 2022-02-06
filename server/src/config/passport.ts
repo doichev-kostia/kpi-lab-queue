5dfc877a89ef4d2c146f4644cf65977e88e532df
doichev-kostia <kostynd2004@gmail.com>
2022-02-06 23:11:29 +0200

    fix: passport config

--- server/src/config/passport.ts
+++ server/src/config/passport.ts
@@ -1,38 +1,42 @@
 import {ExtractJwt, Strategy, StrategyOptions} from "passport-jwt";
 import {models} from "../models";
-import {Authenticator} from "passport";
+import {Roles} from "../types";
+import passport from "passport";
+import { config } from "dotenv";
+config();
 
-export default async (passport: Authenticator) => {
+export const configurePassport = (passport: passport.PassportStatic) => {
     const options: Partial<StrategyOptions> = {};
-
     options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-    options.secretOrKey = process.env.SECRET;
+    options.secretOrKey = process.env.SECRET || "secret";
 
     passport.use("jwt",
-        new Strategy(options as StrategyOptions, async (jwt_payload, done) => {
+        new Strategy(options as StrategyOptions, async (payload, done) => {
             try {
-                const user = await models.User.findById(jwt_payload.id).exec();
+                const user = await models.User.findById(payload.id);
                 if (!user) {
+                    console.error(`User not found, jwt id: ${payload.id}`)
                     return done(null, false);
                 }
                 return done(null, user);
             } catch (error) {
                 done(error, false);
-                console.log(error);
+                console.error(error);
             }
         })
     );
 
     passport.use("jwt-admin",
-        new Strategy(options as StrategyOptions, async (jwt_payload, done) => {
+        new Strategy(options as StrategyOptions, async (payload, done) => {
             try {
-                const user = await models.User.findById(jwt_payload.id).exec();
+                const user = await models.User.findById(payload.id).exec();
 
                 if (!user) {
+                    console.error(`User not found, jwt id: ${payload.id}`)
                     return done(null, false);
                 }
 
-                if (!user.isAdmin) {
+                if (user.role === Roles.USER) {
                     return done(null, false, {
                         message: "You don't have enough permissions to complete" +
                             " this action"
@@ -42,8 +46,31 @@ export default async (passport: Authenticator) => {
                 done(null, user);
             } catch (error) {
                 done(error, false);
-                console.log(error);
+                console.error(error);
             }
         })
     );
+
+    passport.use("jwt-super-admin", new Strategy(options as StrategyOptions, async (payload, done) => {
+        try {
+            const user = await models.User.findById(payload.id).exec();
+
+            if (!user) {
+                console.error(`User not found, jwt id: ${payload.id}`)
+                return done(null, false);
+            }
+
+            if (user.role !== Roles.SUPER_ADMIN) {
+                return done(null, false, {
+                    message: "You don't have enough permissions to complete" +
+                        " this action"
+                });
+            }
+
+            done(null, user);
+        } catch (error) {
+            done(error, false);
+            console.error(error);
+        }
+    }))
 };
